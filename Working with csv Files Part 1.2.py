86c21e9c7d25596611b5d4bda73ca13695ba9f95
Maksim Kisel <103454376+Waspasik@users.noreply.github.com>
2022-09-29 23:22:23 +0300

    Added one answer on the skipped task

--- Working with csv Files Part 1.2.py
+++ Working with csv Files Part 1.2.py
@@ -234,3 +234,71 @@ with open('titanic.csv', encoding='utf-8') as file:
 
     print(*survived_males, sep='\n')
     print(*survived_females, sep='\n')
+
+    
+    
+# Лог-файл
+# Вам доступен файл name_log.csv, в котором находятся логи изменения имени пользователя.
+# В первом столбце записано измененное имя пользователя, во втором — адрес электронной
+# почты, в третьем — дата и время изменения. При этом email пользователь менять не может,
+# только имя:
+
+# username,email,dtime
+# rare_charles6,charlesthompson@inbox.ru,15/11/2021 08:15
+# busy_patricia5,patriciasmith@bk.ru,07/11/2021 08:07
+# ...
+
+# Напишите программу, которая отбирает из файла name_log.csv только самые свежие записи
+# для каждого пользователя и записывает их в файл new_name_log.csv. В файле new_name_log.csv
+# первой строкой должны быть заголовки столбцов такие же, как в файле name_log.csv. Логи
+# в итоговом файле должны быть расположены в лексикографическом порядке названий электронных
+# ящиков пользователей.
+
+# Примечание 1. Для части пользователей в исходном файле запись только одна, и тогда в
+# итоговый файл следует записать только ее, для некоторых пользователей есть несколько
+# записей с разными именами.
+
+# Например, пользователь с электронной почтой c3po@gmail.com несколько раз менял имя:
+
+# C=3PO,c3po@gmail.com,16/11/2021 17:10
+# C3PO,c3po@gmail.com,16/11/2021 17:15
+# C-3PO,c3po@gmail.com,16/11/2021 17:24
+
+# Из этих трех записей в итоговый файл должна быть записана только одна — самая свежая:
+
+# C-3PO,c3po@gmail.com,16/11/2021 17:24
+
+# Примечание 2. Разделителем в файле name_log.csv является запятая, при этом кавычки
+# не используются.
+
+# Примечание 3. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
+
+# Примечание 4. Начальная часть файла new_name_log.csv выглядит так:
+
+# username,email,dtime
+# angry-barbara2,barbaraanderson@bk.ru,17/11/2021 01:17
+# dead-barbara6,barbarabrown@rambler.ru,27/11/2021 08:27
+# busy_barbara7,barbaradavis@aol.com,24/11/2021 08:24
+# ...
+
+# Примечание 5. При открытии файла используйте явное указание кодировки UTF-8.
+
+import csv
+from datetime import datetime
+
+with open('D:/Python/Stepik/name_log.csv', encoding='utf-8') as file:
+    reader = list(csv.reader(file))
+    reader_dict = {}
+    email_users_dict = {}
+    columns = reader[0]
+    for i in range(1, len(reader)):
+        reader_dict[datetime.strptime(reader[i][-1], '%d/%m/%Y %H:%M')] = reader_dict.get(datetime.strptime(reader[i][-1], '%d/%m/%Y %H:%M'), reader[i][:-1])
+
+    for i in range(1, len(reader)):
+        email_users_dict[reader[i][1]] = email_users_dict.get(reader[i][1], []) + [datetime.strptime(reader[i][-1], '%d/%m/%Y %H:%M')]
+    
+    with open('D:/Python/Stepik/new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+        for key, value in email_users_dict.items():
+            writer.writerow([reader_dict[max(value)], max(value).strftime('%d/%m/%Y %H:%M')])
