1be07d98ae48e050d14fa326bcc2c75504bf3ff7
Maksim Kisel <103454376+Waspasik@users.noreply.github.com>
2022-11-07 11:05:22 +0300

    Added skipped task "Декоратор ignore_exception"

--- Decorators Part 2.py
+++ Decorators Part 2.py
@@ -357,3 +357,48 @@ def beegeek():
 print(beegeek.attr1)
 print(beegeek.attr2)
 print(beegeek.__name__)
+
+
+
+# Декоратор ignore_exception
+# Реализуйте декоратор ignore_exception, который принимает произвольное количество
+# позиционных аргументов — типов исключений, и выводит текст:
+
+# Исключение <тип исключения> обработано
+# если во время выполнения декорируемой функции было возбуждено исключение, принадлежащее
+# одному из переданных типов.
+
+# Если возбужденное исключение не принадлежит ни одному из переданных типов, оно должно
+# быть возбуждено снова.
+
+# Также декоратор должен сохранять имя и строку документации декорируемой функции.
+
+# Примечание 1. Не забывайте про то, что декоратор не должен поглощать возвращаемое
+# значение декорируемой функции, а также должен уметь декорировать функции с произвольным
+# количеством позиционных и именованных аргументов.
+
+import functools
+
+
+def ignore_exception(*exceptions_args):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as err:
+                if type(err) in exceptions_args:
+                    print(f'Исключение {type(err).__name__} обработано')
+                else:
+                    raise
+        return wrapper
+    return decorator
+
+
+@ignore_exception(ValueError, TypeError, NameError)
+def func():
+    '''func docs'''
+    raise ValueError
+ 
+print(func.__name__)
+print(func.__doc__)
